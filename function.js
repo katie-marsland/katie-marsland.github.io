4cb90268146898dd2a310ee45f7f072cb857431a
katie-marsland <katie.marsland@yahoo.ca>
2020-11-24 23:09:10 -0500

    Initial commit

---
+++ function.js
@@ -0,0 +1,25 @@
+
+function #c{
+  transition: all .5s ease-in-out;
+}
+#c:hover{
+  fill:#e4b125;
+  stroke-width: 2;
+  stroke: #4b1972;
+  transform:translateY(-20px);
+}
+#resume{
+  transition: all .5s ease-in-out;
+  color: #4b1972;
+}
+#resume:hover{
+  background-color: #4b1972;
+  transform:translateY(-20px);
+}
+#resumeline{
+  transition: all .5s ease-in-out;
+  color: #4b1972;
+}
+#resumeline:hover{
+  background-color: #4b1972;
+  transform:translateY(-20px);
